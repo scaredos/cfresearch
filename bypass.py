67ee07599812bbe7cccd9e58d69b95312fa336ab
ScaredOS <39132876+scaredos@users.noreply.github.com>
2019-11-03 12:53:41 -0500

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -3,5 +3,5 @@ import random
 s = cfscrape.create_scraper()
 while 1:
     x = str(random.random())
-    print s.post('http://silent-uk.projectkyros.com:19999/'+x).headers
+    print s.post('https://example.com/'+x).headers
 
